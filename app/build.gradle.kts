ee9cdc292050be78320344b5bc8c3f2680581a10
Jakub Westa <jakub.westa@gmail.com>
2025-11-05 20:44:24 +0100

    Added signing, and keystore to gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = "app-alias"
+            keyPassword = "orienteering-password"
+            storeFile = file("keystore.jks")
+            storePassword = "orienteering-password"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,6 +35,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
