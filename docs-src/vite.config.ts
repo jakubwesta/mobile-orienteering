797afd10e7b2baf6dc3d33c2ba2118e94fa64cb2
Jakub Westa <jakub.westa@gmail.com>
2026-01-18 07:58:03 +0100

    Updated vite config to support custom domain

--- docs-src/vite.config.ts
+++ docs-src/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/mobile-orienteering/',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
